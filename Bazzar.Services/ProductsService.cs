ba5063d422459b3f1eed7a08063f25381ce29f33
Sameer Tanveer <126457978+sameertanveer-techno-soft@users.noreply.github.com>
2023-03-09 20:02:07 +0500

    product-listing

---
+++ Bazzar.Services/ProductsService.cs
@@ -0,0 +1,52 @@
+﻿using Bazzar.Database;
+using Bazzar.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bazzar.Services
+{
+    public class ProductService
+    {
+        public List<Product> ViewProduct()
+        {
+            using (var context = new BazzarContext())
+            {
+                return context.Products.ToList();
+                
+            }
+        }
+        public Product GetProductById(int Id)
+        {
+            using (var context = new BazzarContext())
+            {
+                return context.Products.Find(Id);
+
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new BazzarContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void SaveProduct(Product product)
+        {
+            using (var context = new BazzarContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void DeleteProduct(int id)
+        {
+            using (var context = new BazzarContext())
+            {
+                var product = context.Products.Find(id);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Bazzar.Web/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Bazzar.Entities;
+using Bazzar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bazzar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductService productService = new ProductService();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listing()
+        {
+            var products = productService.ViewProduct().ToList();
+            return View(products);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+
+            productService.SaveProduct(product);
+            return RedirectToAction("Listing");
+        }
+    }
+}
